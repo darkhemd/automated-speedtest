552c108f50fa439dfeecb2a060edeb006efa3416
darkhemd <41227999+darkhemd@users.noreply.github.com>
2022-05-02 14:24:04 +0200

    Update speedtest.sh
    added a check if speedtest is installed on the system

--- speedtest.sh
+++ speedtest.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
+
+if ! command -v speedtest &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install curl
+    curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+    sudo apt-get install speedtest
+fi
+
 declare -i x=0
 while true
 do
-echo Test $x
-dt=$(date '+%d/%m/%Y %H:%M:%S');
-speedtest -s 31470 -f csv>>Speedtest.log
-echo "\"$dt\"">>SpeedtestDate.log
-let "x+=1"
-sleep 600
+    echo Test $x
+    dt=$(date '+%d/%m/%Y %H:%M:%S');
+    speedtest -s 31470 -f csv>>Speedtest.log
+    echo "\"$dt\"">>SpeedtestDate.log
+    let "x+=1"
+    sleep 600
 done
